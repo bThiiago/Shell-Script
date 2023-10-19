3f13ebaf06038eb1591c7f91bca65214149965ee
zFranca1 <joaopedro.franca1@hotmail.com>
2023-10-18 22:22:36 -0300

    comentando alguns arquivos

--- Codigos/extracao_dados_internet/extrai_titulos.sh
+++ Codigos/extracao_dados_internet/extrai_titulos.sh
@@ -9,6 +9,8 @@ VERMELHO="\033[31;1m"
 #debian like
 #[ ! -x "$(which lynx)" ] && sudo apt install lynx -y # Lynx instalado?
 
+                                            #substituindo tudo que começa com div e termina com line, por nada
+                                            #depois tudo oq começa com span e tenha qualquer coisa.
 lynx -source http://lxer.com/ | grep blurb | sed 's/<div.*line">//;s/<\/span.*//' > titulos.txt
 
 while read -r titulo_lxer
